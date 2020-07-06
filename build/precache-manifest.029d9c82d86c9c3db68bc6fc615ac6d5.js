11ce681134aeeee506f1b6b2542804519d2fe65d
Abanoub <bebomagdy666@gmail.com>
2020-07-06 19:54:24 +0200

    ready to deploy

---
+++ build/precache-manifest.029d9c82d86c9c3db68bc6fc615ac6d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52ba031dfea5830f7e86c11f37ac016b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5b88df7c7ac07d8259e",
+    "url": "/static/css/2.c2bc211b.chunk.css"
+  },
+  {
+    "revision": "825758b7eac1d2da707e",
+    "url": "/static/css/main.0b7be2b9.chunk.css"
+  },
+  {
+    "revision": "b5b88df7c7ac07d8259e",
+    "url": "/static/js/2.cdc01c18.chunk.js"
+  },
+  {
+    "revision": "5dbf9415e481a23d65662fd7dec5f7a6",
+    "url": "/static/js/2.cdc01c18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "825758b7eac1d2da707e",
+    "url": "/static/js/main.82e28a96.chunk.js"
+  },
+  {
+    "revision": "3564682db313855a2530",
+    "url": "/static/js/runtime-main.2d0394c2.js"
+  }
+]);
